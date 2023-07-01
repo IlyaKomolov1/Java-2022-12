8e0d771bd71f3edce8a6e1f9b2a8c301b6a813a0
IlyaKomolov1 <komolovilu@mail.ru>
2023-07-01 23:38:21 +0300

    HomeWork4

---
+++ hw04-gc/src/main/java/org/otus/Data.java
@@ -0,0 +1,15 @@
+package org.otus;
+
+public class Data {
+    private  Integer value;
+
+    public Data(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
+
+
